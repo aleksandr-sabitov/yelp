d6ca9e284f331572db6327817baa43202ef7fcda
asabitov <aleksandr.sabitov@zalando.de>
2019-01-21 09:36:06 +0100

    some clean upmm

--- app/app.sh
+++ app/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 date
 mkdir /tmp/yelp
-echo "Exctracting JSONs from Yelp tar file..."
+echo "Exctracting JSONs from Yelp tar file (few minutes)..."
 tar -C /tmp/yelp -xvf /tmp/yelp.tar
 echo "Converting JSONs to Parquet (few minutes...) and opening Spark SQL session..."
 spark/bin/spark-sql -S -f /tmp/convert_to_parquet.sql
